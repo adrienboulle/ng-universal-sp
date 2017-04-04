095250db3be48b78e1d48ca6536b22bd00e9caf2
Adrien Boullé <adrien.boulle@gadz.org>
2017-04-04 21:24:24 +0200

    run ngc twice to fix build

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ if ! [ $env ]; then
   env=dev
 fi
 
+node_modules/.bin/ngc
+node_modules/.bin/ngc
+
 node_modules/.bin/webpack --config webpack.client.config.js
 node_modules/.bin/webpack --config webpack.server.config.js
 node_modules/.bin/webpack --config src/app/lazy/webpack.lazy.config.js
