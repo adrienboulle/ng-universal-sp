261790aac70199dab7151ed876b8939adfedaddc
Adrien Boullé <adrien.boulle@gadz.org>
2017-04-04 21:26:04 +0200

    fix path

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -3,7 +3,7 @@ const shared = require('./webpack/shared');
 module.exports = {
   entry: {
     home: './built/src/app/client.js',
-    login: './built/src/login/app.browser.js',
+    login: './built/src/login/client.js',
   },
   output: shared.output,
   module: {
